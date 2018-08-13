a591ddb4852c80bec5a58138e7b9304138eb7918
Holiday Course <holiday@ad.aie.edu>
2018-08-13 16:23:47 -0500

    Add project files.

---
+++ Intro Cpp/Main.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+
+int main()
+{
+	std::cout << "Devyn Williamson" << std::endl;
+	std::cout << "mango" << std::endl;
+	std::cout << "green apple" << std::endl;
+	std::cout << "pineapple" << std::endl;
+
+	std::cout << std::endl;
+
+	std::cout << "01. Thnks fr th mmrs" << std::endl;
+	std::cout << "02. Awake and Alive" << std::endl;
+	std::cout << "03. Toxicity" << std::endl;
+	std::cout << "04. The Resistence" << std::endl;
+	std::cout << "05. Sugar, We're Goin Down" << std::endl;
+	std::cout << "05. Misery Business" << std::endl;
+	std::cout << "06. Natural" << std::endl;
+	std::cout << "07. Hero" << std::endl;
+	std::cout << "08. Chop Suey" << std::endl;
+	std::cout << "09. Feel Invincible" << std::endl;
+	std::cout << "10. Ignorance" << std::endl;
+
+	std::cout << std::endl;
+
+	std::cout << "0" << std::endl;
+	std::cout << "1" << std::endl;
+	std::cout << "2" << std::endl;
+	std::cout << "3" << std::endl;
+	std::cout << "4" << std::endl;
+	std::cout << "5" << std::endl;
+
+	std::cout << std::endl;
+
+	std::cout << "5" << std::endl;
+	std::cout << "4" << std::endl;
+	std::cout << "3" << std::endl;
+	std::cout << "2" << std::endl;
+	std::cout << "1" << std::endl;
+	std::cout << "0" << std::endl;
+
+	system("Pause");
+	return 0;
+}
